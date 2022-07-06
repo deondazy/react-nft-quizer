e6ec73a19472ff5554062e879e73f4c46641ce02
deondazy <deondazy@gmail.com>
2022-07-06 01:04:15 +0100

    Diferent result based on quiz score

--- src/components/QuizResult.tsx
+++ src/components/QuizResult.tsx
@@ -1,9 +1,21 @@
+function claimReward() {
+    return alert("Reward");
+}
+
 function QuizResult({result, retry}: {result: any, retry: any}) {
     return (
         <div className="result-screen">
             <h2>Result: <strong>{result.percentage}%</strong></h2>
             <p>Selected <strong>{result.correct}</strong> correct options out of <strong>{result.total}</strong> questions.</p>
-            <button onClick={retry}>Retry</button>
+
+            {result.correct < 7 ? (
+                <button onClick={retry}>Retry</button>
+            ) : (
+                <>
+                <p><strong>Congratulations! You have passed the quiz!</strong></p>
+                <button onClick={claimReward}>Claim reward</button>
+                </>
+            )}
         </div>
     );
 }
